eac392d1729a6609ab357dd20a4cda6d920a7886
Alexis Ken Alvarez <80466376+AlexisKenAlvarez@users.noreply.github.com>
2022-06-26 21:29:44 +0800

    Add files via upload
    Not yet done.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alvarez</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon-32x32.png" />
+    <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <nav>
+        <div class="navContainer">
+            <img src="img/logo-aki-Copy.webp" alt="Logo" class="myLogo">
+        </div>
+    </nav>
+    <section>
+        <div class="hero">
+            <div class="soc">
+                <div class="whiteLine">
+                </div>
+                <div class="socMeds">
+                    <a href="" class="socLink"><img src="img/socPort/001-facebook.svg" alt="Facebook" class="imgSoc"></a>
+                    <a href="" class="socLink"><img src="img/socPort/011-instagram.svg" alt="Instagram" class="imgSoc"></a>
+                    <a href="" class="socLink"><img src="img/socPort/010-linkedin.svg" alt="linkedin" class="imgSoc"></a>
+                    <a href="" class="socLink"><img src="img/socPort/icons8-github-60.svg" alt="github" class="imgSoc git"></a>
+                </div>
+            </div>
+            <div class="mainContainer">
+                <div class="bio">
+                    <h1>Alexis Ken Alvarez</h1>
+                    <p>Aspiring Full Stack Web Developer</p>
+                    <div class="download">
+                        Download CV
+                    </div>
+                </div>
+                <img src="img/newFordaWebsite.webp" alt="Profile" class="myImage">
+            </div>
+
+        </div>
+    </section>
+</body>
+</html>
