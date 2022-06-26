4247c2045c8d4c2d3811ef05dd9c043b4dfee9d9
Alexis Ken Alvarez <80466376+AlexisKenAlvarez@users.noreply.github.com>
2022-06-26 22:47:47 +0800

    Add files via upload
    Homepage done

---
+++ index.js
@@ -0,0 +1,25 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({ defaults: { opacity: 0, duration: 2, y: 1000, ease: 'Power4.easeInOut' } });
+var iconTL = gsap.timeline({ defaults: { opacity: 0, duration: 2, y: 100, ease: 'circ.easeInOut' } });
+
+
+var line = gsap.from('.whiteLine', { opacity: 0, duration: 2, y: 100, ease: 'circ.easeInOut' }, '-=6.6');
+iconTL.add(line);
+
+var headers = gsap.timeline({ defaults: { opacity: 0, duration: 1.3, y: -50, ease: 'circ.easeInOut' } });
+headers.from('.alexis', {})
+    .from('.myLogo', {}, "-=1")
+    .from('.aspire', {}, "-=1")
+    .from('.download', {}, "-=1")
+
+gsap.from('.myImage', { opacity: -1, duration: 4, y: 100, ease: 'circ.easeInOut' }, "-=1");
+
+document.querySelectorAll('.socLink').forEach(function (icon) {
+    iconTL.from(icon, {}, '-=1.5')
+})
+tl.from('.div1', {})
+    .from('.div2', {}, '-=1.7')
+    .from('.div3', {}, '-=1.7')
+    tl.add(headers)
+    tl.add(iconTL);
